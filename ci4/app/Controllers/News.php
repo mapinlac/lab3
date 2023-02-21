a637d34c21607dc2ddd14a958eb86573e856c87b
Matthew Aaron Pinlac <119918170+mapinlac@users.noreply.github.com>
2023-02-21 15:16:15 +0800

    haha more debugging

--- ci4/app/Controllers/News.php
+++ ci4/app/Controllers/News.php
@@ -7,7 +7,19 @@ use CodeIgniter\Exceptions\PageNotFoundException;
 
 class News extends BaseController
 {
-    // ...
+    public function index()
+    {
+        $model = model(NewsModel::class);
+
+        $data = [
+            'news'  => $model->getNews(),
+            'title' => 'News archive',
+        ];
+
+        return view('templates/header', $data)
+            . view('news/index')
+            . view('templates/footer');
+    }
 
     public function view($slug = null)
     {
