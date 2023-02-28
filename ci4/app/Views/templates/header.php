db6a942a30e4c1c0873931b3729227bff48b1be8
Matthew Aaron Pinlac <119918170+mapinlac@users.noreply.github.com>
2023-03-01 00:26:07 +0800

    update website major

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,13 +1,10 @@
 <!doctype html>
 <html>
 <head>
-<?php echo link_tag ('css/style.css') ?>
-<?php echo script_tag ('js/javascript.js') ?>
+<?php echo link_tag ('css/styles.css') ?>
+<?php echo script_tag ('js/scripts.js') ?>
     <title>CodeIgniter Tutorial</title>
 </head>
 <body>
-    <h1><?= esc($title) ?></h1>
-	<div class="sticky"><h1><center>Introductions!</center></h1>
-	</div>
 </body>
 </html>
